8b706c49dc65a432894213ba5f6c5d48335ffb77
khizarali07 <khizarali.cs@gmail.com>
2025-08-10 16:26:43 +0500

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      // Disable this rule so apostrophes in JSX don't fail builds
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
 
 export default eslintConfig;
